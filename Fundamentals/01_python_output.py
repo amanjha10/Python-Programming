d88e91f324f7ffbe44a7bbbcb9611eee265cb32c
Aman Jha <amanjha3232@gmail.com>
2024-01-10 21:29:59 +0545

    first commit

---
+++ Fundamentals/01_python_output.py
@@ -0,0 +1,31 @@
+# Python is a case sensitive language
+print('Hello World')
+
+print('fundamentals of python')
+
+#print(fundamentals of python ) ----> You will get an error as strings are always written inside quotes. 
+
+
+# Numbers can be printed without inverted commas !
+print(10)
+print(8)
+
+# Boolean 
+print(True)
+print(False)
+
+#Floating numbers
+print(5.2)
+print(6.3)
+
+# You can also use multiple data types inside a same print ()
+print('Hello',1,4.5,True)
+print('Hello',1,4.5,True,sep='/')
+
+# by default end value is set to a new line 
+print('hello')
+print('world')
+
+#end ='-'
+print('hello',end='-')
+print('world')
---
+++ Fundamentals/02_data_types.py
@@ -0,0 +1,33 @@
+# Integer
+print(8)
+# 1*10^308
+print(1e309)
+
+# Decimal/Float
+print(8.55)
+print(1.7e309)
+
+# Boolean
+print(True)
+print(False)
+
+# Text/String
+print('Hello World')
+
+# complex
+print(2+3j)
+
+# List-> C-> Array
+print([1,3,5,7,9])
+
+# Tuple
+print((1,3,5,7,9))
+
+# Sets
+print({1,2,3,4,5})
+
+# Dictionary
+print({'name':'Aman','gender':'Male','weight':73})
+
+# type
+type([1,2,3])
