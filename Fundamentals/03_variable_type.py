cca948374e55e49335f238988fdf646d9009237c
Aman Jha <amanjha3232@gmail.com>
2024-01-11 21:49:38 +0545

    variable and keywords

---
+++ Fundamentals/03_variable_type.py
@@ -0,0 +1,46 @@
+# Static Vs Dynamic Typing
+# Static Vs Dynamic Binding
+# stylish declaration techniques
+
+# C/C++
+name = 'python'
+print(name)
+
+c = 5
+b = 6
+
+print(c + b)
+
+# Dynamic Typing
+a = 5
+# Static Typing
+# int a = 5
+
+# Dynamic Binding
+a = 5
+print(a)
+a = 'nitish'
+print(a)
+
+# Static Binding
+# int a = 5
+
+a = 1
+b = 2
+c = 3
+print(a,b,c)
+
+a,b,c = 1,2,3
+print(a,b,c)
+
+a=b=c= 5
+print(a,b,c)
+
+# Comments
+
+# this is a comment
+# second line
+a = 4
+b = 6 # like this
+# second comment
+print(a+b)
---
+++ Fundamentals/04_Keyword_identifiers.py
@@ -0,0 +1,25 @@
+# Identifiers
+# You can't start with a digit
+name1 = 'python'
+print(name1)
+# You can use special chars -> _
+_ = 'python'
+print(_)
+# identiers can not be keyword
+
+# Temp Heading 
+
+# User Input 
+
+#Static Vs Dynamic
+input('Enter Email')
+
+# take input from users and store them in a variable
+fnum = int(input('enter first number'))
+snum = int(input('enter second number'))
+#print(type(fnum),type(snum))
+# add the 2 variables
+result = fnum + snum
+# print the result
+print(result)
+print(type(fnum))
