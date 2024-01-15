4d61809b5c14a1c27de3a3b74147cbbbfe79ee3a
Aman Jha <amanjha3232@gmail.com>
2024-01-15 22:20:08 +0545

    Sub String

---
+++ Strings/02_substring.py
@@ -0,0 +1,16 @@
+# Positive Indexing
+s = 'hello world'
+print(s[41])
+
+# Negative Indexing
+s = 'hello world'
+print(s[-3])
+
+# Slicing
+s = 'hello world'
+print(s[6:0:-2])
+
+print(s[::-1])
+
+s = 'hello world'
+print(s[-1:-6:-1])
