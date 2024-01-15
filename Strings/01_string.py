8d0e480041358fde27bf26ca5c92dbbd76012873
Aman Jha <amanjha3232@gmail.com>
2024-01-15 22:18:26 +0545

    String

---
+++ Strings/01_string.py
@@ -0,0 +1,21 @@
+# Strings are sequence of Characters
+
+# In Python specifically, strings are a sequence of Unicode Characters
+
+# Creating Strings
+# Accessing Strings
+# Adding Chars to Strings
+# Editing Strings
+# Deleting Strings
+# Operations on Strings
+# String Functions
+
+s = 'hello'
+s = "hello"
+# multiline strings
+s = '''hello'''
+s = """hello"""
+s = str('hello')
+print(s)
+
+"it's raining outside"
