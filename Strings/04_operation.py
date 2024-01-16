e62b61becba2eec1200c69281eb3463fa2144b9b
Aman Jha <amanjha3232@gmail.com>
2024-01-16 21:55:02 +0545

    Operation on string

---
+++ Strings/04_operation.py
@@ -0,0 +1,41 @@
+# Operations on Strings
+
+
+# Arithmetic Operations
+# Relational Operations
+# Logical Operations
+# Loops on Strings
+# Membership Operations
+
+print('kathmandu' + ' ' + 'nepal')
+
+print('nepal'*5)
+
+print("*"*50)
+
+'nepal' != 'nepal'
+
+# lexiographically
+'nepal' > 'kathmandu'
+
+'Nepal' > 'nepal'
+
+'hello' and 'world'
+
+'hello' or 'world'
+
+'' and 'world'
+
+'' or 'world'
+
+'hello' or 'world'
+
+not 'hello'
+
+for i in 'hello':
+  print(i)
+
+for i in 'delhi':
+  print('pune')
+
+'D' in 'delhi'
