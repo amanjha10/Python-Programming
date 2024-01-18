cc75622fade217e7808345666c7d25b0069a8289
Aman Jha <amanjha3232@gmail.com>
2024-01-18 21:25:51 +0545

    Ends/starts with and format

---
+++ Strings/08_esf.py
@@ -0,0 +1,12 @@
+# endswith/startswith
+
+print('my name is aman'.endswith('jha'))
+
+print('my name is aman'.startswith('1my'))
+
+# format
+
+name = 'aman'
+gender = 'male'
+
+print('Hi my name is {1} and I am a {0}'.format(gender,name))
