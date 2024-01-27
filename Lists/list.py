0f91386c659c9ee326157f7fe2bcdcd25511345a
Aman Jha <amanjha3232@gmail.com>
2024-01-27 19:15:39 +0545

    Creating List and Accessing Item From a List

---
+++ Lists/list.py
@@ -0,0 +1,28 @@
+# Creating a List
+
+
+# Empty
+print([])
+# 1D -> Homo
+print([1,2,3,4,5])
+# 2D
+print([1,2,3,[4,5]])
+# 3D
+print([[[1,2],[3,4]],[[5,6],[7,8]]])
+# Hetrogenous
+print([1,True,5.6,5+6j,'Hello'])
+# Using Type conversion
+print(list('hello'))
+
+# Accessing Items from a List
+
+
+# Indexing
+L = [[[1,2],[3,4]],[[5,6],[7,8]]]
+#positive
+#print(L[0][0][1])
+
+# Slicing
+L = [1,2,3,4,5,6]
+
+print(L[::-1])
