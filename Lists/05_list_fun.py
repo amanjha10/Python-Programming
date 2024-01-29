631c9fb08443b61ace5b1705c5e7778caf4f2c95
Aman Jha <amanjha3232@gmail.com>
2024-01-29 19:54:56 +0545

    Lists Functions

---
+++ Lists/05_list_fun.py
@@ -0,0 +1,39 @@
+# List Functions
+
+# len/min/max/sorted
+L = [2,1,5,7,0]
+
+print(len(L))
+print(min(L))
+print(max(L))
+print(sorted(L,reverse=True))
+
+# count 
+L = [1,2,1,3,4,1,5]
+L.count(5)
+
+# index
+L = [1,2,1,3,4,1,5]
+L.index(1)
+
+# reverse
+L = [2,1,5,7,0]
+# permanently reverses the list
+L.reverse()
+print(L)
+
+# sort (vs sorted)
+L = [2,1,5,7,0]
+print(L)
+print(sorted(L))
+print(L)
+L.sort()
+print(L)
+
+# copy -> shallow
+L = [2,1,5,7,0]
+print(L)
+print(id(L))
+L1 = L.copy()
+print(L1)
+print(id(L1))
