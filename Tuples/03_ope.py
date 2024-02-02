be2217470d14109801400eb14fcc2266f2180b8b
Aman Jha <amanjha3232@gmail.com>
2024-02-02 22:21:20 +0545

    Operation and Tuple Function

---
+++ Tuples/03_ope.py
@@ -0,0 +1,36 @@
+# + and *
+t1 = (1,2,3,4)
+t2 = (5,6,7,8)
+
+print(t1 + t2)
+
+print(t1*3)
+# membership
+1 in t1
+# iteration
+for i in t1:
+  print(i)
+
+# Tuple Functions
+  
+
+# len/sum/min/max/sorted
+t = (1,2,3,4)
+len(t)
+
+sum(t)
+
+min(t)
+
+max(t)
+
+sorted(t,reverse=True)
+
+# count
+
+t = (1,2,3,4,5)
+
+t.count(50)
+
+# index
+t.index(50)
